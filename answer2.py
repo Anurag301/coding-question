2b1fc9291175d04cd442dfedaffd635ddf4c047e
Anurag301 <115408889+Anurag301@users.noreply.github.com>
2022-12-07 22:24:17 +0530

    Add files via upload

---
+++ answer2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+rows = int(input("Enter number of rows: "))
+
+for i in range(rows):
+    for j in range(i+1):
+        print("* ", end="")
+    print("\n")
+
+
+# In[ ]:
+
+
+
+
